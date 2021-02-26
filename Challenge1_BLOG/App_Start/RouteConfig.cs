c0bac0725aa9e9d28e0da48d76d2b3bf50fa6703
hernanVillalba <63215609+hernanVillalba@users.noreply.github.com>
2021-02-26 12:41:59 -0300

    functionality (details and add)

--- Challenge1_BLOG/App_Start/RouteConfig.cs
+++ Challenge1_BLOG/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Challenge1_BLOG
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GET", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Challenge1_BLOG/Controllers/GETController.cs
+++ Challenge1_BLOG/Controllers/GETController.cs
@@ -13,11 +13,14 @@ namespace Challenge1_BLOG.Controllers
         Database db = new Database();
 
         //GET: Blog
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string message)
         {
+            ViewBag.Message = message;
             return View(db.Tabla.ToList());
         }
 
+        [HttpGet]
         public ActionResult Detalles(int id)
         {
             var blog = new Tabla();
@@ -32,10 +35,55 @@ namespace Challenge1_BLOG.Controllers
                 //entonces igualo la varibale 'blog' a null para mostrar el error en la view.
                 blog = null;
             }
-            
+
             return View(blog);
         }
 
+        [HttpGet]
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        /*
+        //public ActionResult Agregar(string Titulo, string Contenido, string Imagen, string Categoria, DateTime Fecha_Creacion)
+        ViewBag.Titulo = Titulo;
+            ViewBag.Contenido = Contenido;
+            ViewBag.Imagen = Imagen;
+            ViewBag.Categoria = Categoria;
+            ViewBag.Fecha_Creacion = Fecha_Creacion;
+        */
+        [HttpPost]
+        public ActionResult Agregar(FormCollection formAgregar)
+        {
+            Tabla tabla = new Tabla();
+            string message; // para mostrar si los datos fueron guardados correctamente.
+
+            try
+            {
+                tabla.Titulo = formAgregar["Titulo"];
+                tabla.Contenido = formAgregar["Contenido"];
+                tabla.Imagen = formAgregar["Imagen"];
+                tabla.Categoria = formAgregar["Categoria"];
+                tabla.Fecha_Creacion = Convert.ToDateTime(formAgregar["Fecha_Creacion"]);
+                db.Tabla.Add(tabla);
+
+                if (db.SaveChanges() == 1)
+                {
+                    message = "Datos guardados correctamente.";
+                }
+                else
+                {
+                    message = "No se pudo guardar los datos.";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = "Exepcion generada. Detalles: " + ex.Message;
+            }
+
+            return RedirectToAction("Index","GET", new { message });
+            //return View();
+        }
 
     }
 }
